dbdb5d0c66759741674f4ac2f2a457953781c64a
OscarHdez103 <iu22229@bristol.ac.uk>
2023-10-25 15:15:52 +0100

    .

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
+	server := flag.String("server", "3.91.227.58:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
 	//TODO: connect to the RPC server and send the request(s)
